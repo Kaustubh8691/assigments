497e53a8a69a213c129ce69f4d155d7922dc298a
Kaustubh8691 <kaustubhnangarepatil8691@gmail.com>
2022-02-06 10:37:18 +0530

    assignment-2-submi

--- src/Assignment-2/assignment2_test.js
+++ src/Assignment-2/assignment2_test.js
@@ -15,7 +15,7 @@
 
   // Define a test suite using Mocha's standard `define` function.
   describe.only("Assignment-2", function () {
-    // Variables used by our tests. They're populated in the `before()` and `beforeEach()` functions.
+    // Variables used by our tests. They're populated  in the `before()` and `beforeEach()` functions.
     var frame; // The Quixote test frame.
     var container, frameDom; // The figure element inside the media object. (The icon.)
     var header_ele, form_ele, input; // The content element inside the media object. (The paragraph.)
